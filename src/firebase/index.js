857df1d01b5c5fc942069ad2e9acb46d8e4e9bb3
TonyFreelance <tony@tonyfreelance.com>
2016-08-14 21:51:36 +0700

    Fix the Not found config folder error

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,18 @@
 import firebase from 'firebase';
 
+let env;
 if (process.env.NODE_ENV === 'development') {
-  import env from '../config/env';
+    env = require('../config/env');
 }
 
 try {
-  const config = {
-    apiKey: process.env.REACT_APP_API_KEY || env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN ||env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL || env.REACT_APP_DATABASE_URL,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET || env.REACT_APP_STORAGE_BUCKET,
-  };
-  firebase.initializeApp(config);
+    const config = {
+        apiKey: process.env.REACT_APP_API_KEY || env.REACT_APP_API_KEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN || env.REACT_APP_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_DATABASE_URL || env.REACT_APP_DATABASE_URL,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET || env.REACT_APP_STORAGE_BUCKET,
+    };
+    firebase.initializeApp(config);
 } catch (e) {}
 
 export const githubProvider = new firebase.auth.GithubAuthProvider();
