c9420935ef0f2a9e0757ab48839b40bb87830c30
TonyFreelance <tony@tonyfreelance.com>
2016-08-14 21:32:29 +0700

    Setup the env variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
+import env from '../config/env';
 
 try {
   const config = {
-    apiKey: "AIzaSyB4abnQTHjS6oUQr4DpUXpNOU19DPhWcnM",
-    authDomain: "recipebox-75174.firebaseapp.com",
-    databaseURL: "https://recipebox-75174.firebaseio.com",
-    storageBucket: "recipebox-75174.appspot.com",
+    apiKey: process.env.REACT_APP_API_KEY || env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN ||env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL || env.REACT_APP_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET || env.REACT_APP_STORAGE_BUCKET,
   };
   firebase.initializeApp(config);
 } catch (e) {}
