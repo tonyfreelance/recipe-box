50f8a515f3fef9740f4b9743e40aff98ce0a0bae
TonyFreelance <tony@tonyfreelance.com>
2016-08-19 15:18:32 +0700

    Change to deploy to production

---
+++ src/config/development.env.js
@@ -0,0 +1,8 @@
+let env = {
+    REACT_APP_API_KEY: 'AIzaSyB4abnQTHjS6oUQr4DpUXpNOU19DPhWcnM',
+    REACT_APP_AUTH_DOMAIN: 'recipebox-75174.firebaseapp.com',
+    REACT_APP_DATABASE_URL: 'https://recipebox-75174.firebaseio.com',
+    REACT_APP_STORAGE_BUCKET: 'recipebox-75174.appspot.com'
+};
+
+module.exports = env;
